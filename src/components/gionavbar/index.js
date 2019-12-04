d71da1f437d0fb1a38b9a51aee92ac035eac03d3
ernestognw <ernestognw@gmail.com>
2019-12-04 12:07:04 -0600

    Fixed Navbar

--- src/components/gionavbar/index.js
+++ src/components/gionavbar/index.js
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
-import Navbar from "react-bootstrap/navbar";
-import Nav from "react-bootstrap/nav";
+import Navbar from "react-bootstrap/lib/Navbar";
+import Nav from "react-bootstrap/lib/Nav";
+import NavItem from "react-bootstrap/lib/NavItem";
 
 class GioNavbar extends Component {
-	render() {
-		return (
-			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-				<Navbar.Brand href="#about">Jorge Giovannetti</Navbar.Brand>
-				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav className="ml-auto">
-						<Nav.Link eventKey={1} href="/">
-							About Me
-						</Nav.Link>
-						<Nav.Link eventKey={2} href="#/portfolio">
-							Portfolio
-						</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
-		);
-	}
+  render() {
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#about">Jorge Giovannetti</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <NavItem eventKey={1} href="/">
+              About Me
+            </NavItem>
+            <NavItem eventKey={2} href="#/portfolio">
+              Portfolio
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
 export default GioNavbar;
--- src/views/about/elements.js
+++ src/views/about/elements.js
@@ -1,43 +1,43 @@
 import styled from "styled-components";
-import Row from "react-bootstrap/row";
-import Button from "react-bootstrap/button";
+import Row from "react-bootstrap/lib/Row";
+import Button from "react-bootstrap/lib/Button";
 
 const Container = styled.div`
-	padding: 10px;
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: space-around;
-	background: #f0f0f0;
+  padding: 10px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  background: #f0f0f0;
 `;
 
 const GioRow = styled(Row)`
-	margin: 5px;
-	border-radius: 5px;
-	padding: 5px;
-	width: 100%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  margin: 5px;
+  border-radius: 5px;
+  padding: 5px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const GioButton = styled(Button)`
-	margin: 5px;
-	border-radius: 5px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	color: white !important;
+  margin: 5px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white !important;
 `;
 const BodyText = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 65%;
-	margin: 0 auto;
-	padding: 5px;
-	text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 65%;
+  margin: 0 auto;
+  padding: 5px;
+  text-align: center;
 `;
 
 export { Container, GioRow, GioButton, BodyText };
