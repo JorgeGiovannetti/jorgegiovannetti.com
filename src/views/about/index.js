2b568893974515067a15f6a6a78a29cc5d46ca48
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-01-18 19:44:10 -0600

    Switched to AntD

--- src/views/about/index.js
+++ src/views/about/index.js
@@ -1,59 +1,40 @@
 import React, { Component, Fragment } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Image from "react-bootstrap/image";
 import GioNavbar from "../../components/gionavbar";
+import { Button } from "antd";
+import { Row } from "antd";
+import { Layout } from "antd";
+import { Icon } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faLinkedin,
 	faSpotify,
 	faGithub
 } from "@fortawesome/free-brands-svg-icons";
-import { Container, GioRow, GioButton, BodyText } from "./elements";
+
 class AboutPage extends Component {
 	render() {
 		return (
 			<Fragment>
-				<style>{"body { background-color: #f0f0f0 }"}</style>
 				<GioNavbar></GioNavbar>
-				<Container>
-					<GioRow style={{ flexDirection: "column" }}>
-						<h1 style={{ fontWeight: "600" }}>Jorge Giovannetti</h1>
-						<h3
-							style={{
-								color: "#ffc107",
-								fontStyle: "italic",
-								fontWeight: "600"
-							}}
-						>
-							Programmer and musician
-						</h3>
-					</GioRow>
-					<GioRow style={{ flexDirection: "column" }}>
-						<Image
-							style={{ height: "auto", width: "150px", marginBottom: "7px" }}
-							src={require("./GioProfilePic.jpg")}
-							roundedCircle
-						/>
-						<h2>About Me</h2>
-						<BodyText>
-							I'm a 2nd year computer science student at Tecnológico de
-							Monterrey with a passion for music production and learning. My
-							main areas of interest include computer graphics, artificial
-							intelligence, UX, and web development.
-						</BodyText>
-					</GioRow>
-					<GioRow>
-						<GioButton
-							variant="warning"
+				<Layout type="flex" align="middle">
+					<Row>
+						<Icon></Icon>
+					</Row>
+					<Row>
+						<Button
+							type="primary"
 							href="https://drive.google.com/file/d/1gANwTHqRcKbsLY0w4doSW7wEBnkEyWAH/view?usp=drive_open"
+							rel="noopener"
 							target="_blank"
 						>
-							View resume
-						</GioButton>
-					</GioRow>
-					<GioRow>
-						<GioButton
-							variant="primary"
+							View Resume
+						</Button>
+					</Row>
+					<Row>
+						<Button
+							type="primary"
 							href="https://www.linkedin.com/in/jorgegiovannetti/"
+							rel="noopener"
 							target="_blank"
 						>
 							<FontAwesomeIcon
@@ -61,10 +42,11 @@ class AboutPage extends Component {
 								style={{ marginRight: "3px" }}
 							/>
 							LinkedIn
-						</GioButton>
-						<GioButton
-							variant="success"
+						</Button>
+						<Button
+							type="primary"
 							href="https://open.spotify.com/artist/3wcBGxsP5MCM5jUPQNoXWC"
+							rel="noopener"
 							target="_blank"
 						>
 							<FontAwesomeIcon
@@ -72,17 +54,18 @@ class AboutPage extends Component {
 								style={{ marginRight: "3px" }}
 							/>
 							Spotify
-						</GioButton>
-						<GioButton
+						</Button>
+						<Button
+							type="primary"
 							variant="dark"
 							href="https://github.com/JorgeGiovannetti"
 							target="_blank"
 						>
 							<FontAwesomeIcon icon={faGithub} style={{ marginRight: "3px" }} />
 							Github
-						</GioButton>
-					</GioRow>
-				</Container>
+						</Button>
+					</Row>
+				</Layout>
 			</Fragment>
 		);
 	}
--- src/views/portfolio/index.js
+++ src/views/portfolio/index.js
@@ -4,19 +4,8 @@ class PortfolioPage extends Component {
 	render() {
 		return (
 			<Fragment>
-				<style>{"body { background-color: #f0f0f0 }"}</style>
 				<GioNavbar></GioNavbar>
-				<div
-					style={{
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-						justifyContent: "center"
-					}}
-				>
-					<h1>Projects</h1>
-					<h2 style={{ color: "#b9541d" }}>Under Construction</h2>
-				</div>
+				<div>Under Construction</div>
 			</Fragment>
 		);
 	}
