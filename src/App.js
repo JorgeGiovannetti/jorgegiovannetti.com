f34500b95f8a3c6b31dd514e001e3754d0bd21e5
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-01-18 19:43:24 -0600

    Change to AntD and routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch, BrowserRouter } from "react-router-dom";
 import PortfolioPage from "./views/portfolio";
 import AboutPage from "./views/about";
+import NotFoundPage from "./views/notfound";
 
 const App = () => {
 	return (
 		<main>
-			<HashRouter>
+			<BrowserRouter>
 				<Switch>
 					<Route path="/" component={AboutPage} exact />
 					<Route path="/about" component={AboutPage} exact />
 					<Route path="/portfolio" component={PortfolioPage} exact />
-					<Route component={Error} />
+					<Route path="/404" component={NotFoundPage} exact />
+					<Redirect to="/404" />
 				</Switch>
-			</HashRouter>
+			</BrowserRouter>
 		</main>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd.css";
 import "typeface-roboto";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
