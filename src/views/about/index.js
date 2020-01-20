061960230a54f866320f10ffbaa41eb85d9c2645
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-01-19 21:28:12 -0600

    Modified to have dark background

--- src/views/about/index.js
+++ src/views/about/index.js
@@ -1,103 +1,87 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, Suspense } from "react";
+import { Row, Col, Layout, Typography } from "antd";
 import GioNavbar from "../../components/gionavbar";
-import { Button, Typography } from "antd";
-import { Row } from "antd";
-import { gold } from "@ant-design/colors";
-import GioProfilePic from "./GioProfilePicSmall.webp";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faLinkedin,
-	faSpotify,
-	faGithub
-} from "@fortawesome/free-brands-svg-icons";
+// import { Button } from "antd";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+// 	faLinkedin,
+// 	faSpotify,
+// 	faGithub
+// } from "@fortawesome/free-brands-svg-icons";
+
+const GioProfilePic = React.lazy(() => import("./components/GioProfilePic"));
+const { Content } = Layout;
+const { Title } = Typography;
 
 class AboutPage extends Component {
 	render() {
 		return (
 			<Fragment>
-				<GioNavbar></GioNavbar>
 				<div
-					align="middle"
 					style={{
-						display: "flex",
-						flexDirection: "column"
+						height: "100vh",
+						background: "#34495e",
+						paddingLeft: "10%",
+						paddingRight: "10%"
 					}}
 				>
-					<Row>
-						<Typography>
-							<h1 style={{ color: gold.primary }}>Jorge Giovannetti</h1>
-						</Typography>
-					</Row>
-					<Row>
-						<Typography>
-							<h2 style={{ fontStyle: "italic" }}>Dev</h2>
-						</Typography>
-					</Row>
-					<Row>
-						<img
-							src={GioProfilePic}
-							style={{ borderRadius: "50%", width: "150px" }}
-							alt="Profile"
-						/>
-					</Row>
-					<Row>
-						<div>
-							2nd year CS student @ Tec de Monterrey. My main areas of interest
-							include computer graphics, artificial intelligence, UX, and web
-							development.
-						</div>
-					</Row>
-					<Row style={{ marginTop: "10px", marginBottom: "10px" }}>
-						<Button
-							type="primary"
-							href="https://drive.google.com/file/d/1gANwTHqRcKbsLY0w4doSW7wEBnkEyWAH/view?usp=drive_open"
-							rel="noopener"
-							target="_blank"
-						>
-							View Resume
-						</Button>
-					</Row>
-					<Row
+					<GioNavbar></GioNavbar>
+
+					<Content>
+						<Row>
+							<Col>
+								<Title>Jorge Giovannetti</Title>
+								<Suspense fallback={<div>Loading...</div>}>
+									<GioProfilePic></GioProfilePic>
+								</Suspense>
+								<p style={{ color: "#8892b0" }}>
+									2nd year CS student @ Tec de Monterrey. My main areas of
+									interest include computer graphics, artificial intelligence,
+									UX, and web development.
+								</p>
+							</Col>
+						</Row>
+						{/* <Row
 						style={{
-							display: "flex",
-							justifyContent: "space-around"
+							display: "flex"
 						}}
-					>
-						<Button
-							type="primary"
-							href="https://www.linkedin.com/in/jorgegiovannetti/"
-							rel="noopener"
-							target="_blank"
 						>
-							<FontAwesomeIcon
-								icon={faLinkedin}
-								style={{ marginRight: "3px" }}
-							/>
-							LinkedIn
+						<Button
+						type="primary"
+						href="https://www.linkedin.com/in/jorgegiovannetti/"
+						rel="noopener"
+						target="_blank"
+						>
+						<FontAwesomeIcon
+						icon={faLinkedin}
+						style={{ marginRight: "3px" }}
+						/>
+						LinkedIn
 						</Button>
 						<Button
-							type="primary"
-							href="https://open.spotify.com/artist/3wcBGxsP5MCM5jUPQNoXWC"
-							rel="noopener"
-							target="_blank"
+						type="primary"
+						href="https://open.spotify.com/artist/3wcBGxsP5MCM5jUPQNoXWC"
+						rel="noopener"
+						target="_blank"
 						>
-							<FontAwesomeIcon
-								icon={faSpotify}
-								style={{ marginRight: "3px" }}
-							/>
-							Spotify
+						<FontAwesomeIcon
+						icon={faSpotify}
+						style={{ marginRight: "3px" }}
+						/>
+						Spotify
 						</Button>
 						<Button
-							type="primary"
-							variant="dark"
-							href="https://github.com/JorgeGiovannetti"
-							target="_blank"
-							rel="noopener"
+						type="primary"
+						variant="dark"
+						href="https://github.com/JorgeGiovannetti"
+						target="_blank"
+						rel="noopener"
 						>
-							<FontAwesomeIcon icon={faGithub} style={{ marginRight: "3px" }} />
-							Github
+						<FontAwesomeIcon icon={faGithub} style={{ marginRight: "3px" }} />
+						Github
 						</Button>
-					</Row>
+					</Row> */}
+					</Content>
 				</div>
 			</Fragment>
 		);
