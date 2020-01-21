8864e16fe27fdb764bdb39a21c5317f867042700
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-01-21 12:40:04 -0600

    Added styled button and styled social media icons

---
+++ src/views/about/elements.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { Button } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const SpotifyButton = styled(Button)`
+	background: green;
+`;
+
+const SocialIcon = styled(FontAwesomeIcon)`
+	margin-right: 3px;
+`;
+
+export { SpotifyButton, SocialIcon };
