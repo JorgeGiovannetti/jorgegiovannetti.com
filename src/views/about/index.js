6a07036a105e40892f6b0d404fb8cf66d2f2a396
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-01-21 22:27:22 -0600

    Syntax

--- src/views/about/index.js
+++ src/views/about/index.js
@@ -24,55 +24,57 @@ class AboutPage extends Component {
 					}}
 				>
 					<Title>Jorge Giovannetti</Title>
-					<PageHeader title="About Me"></PageHeader>
-					<Row type="flex">
-						<Col span={12}>
-							<Paragraph>
-								2nd year CS student @ Tec de Monterrey. My main areas of
-								interest include computer graphics, artificial intelligence, UX,
-								and web development.
-							</Paragraph>
-							<Row type="flex">
-								<Button
-									href="https://drive.google.com/file/d/1gANwTHqRcKbsLY0w4doSW7wEBnkEyWAH/view?usp=drive_open"
-									rel="noopener"
-									target="_blank"
-								>
-									Resume
-								</Button>
-							</Row>
-						</Col>
-						<Col span={12}>
-							<GioProfilePic></GioProfilePic>
-						</Col>
-					</Row>
-					<Row type="flex" style={{ marginTop: "5px" }}>
-						<Button
-							href="https://www.linkedin.com/in/jorgegiovannetti/"
-							rel="noopener"
-							target="_blank"
-						>
-							<SocialIcon icon={faLinkedin} />
-							LinkedIn
-						</Button>
-						<SpotifyButton
-							href="https://open.spotify.com/artist/3wcBGxsP5MCM5jUPQNoXWC"
-							rel="noopener"
-							target="_blank"
-						>
-							<SocialIcon icon={faSpotify} />
-							Spotify
-						</SpotifyButton>
-						<Button
-							variant="dark"
-							href="https://github.com/JorgeGiovannetti"
-							target="_blank"
-							rel="noopener"
-						>
-							<SocialIcon icon={faGithub} />
-							Github
-						</Button>
-					</Row>
+					<div style={{ marginLeft: "5%" }}>
+						<PageHeader title="About Me"></PageHeader>
+						<Row type="flex">
+							<Col span={12}>
+								<Paragraph>
+									2nd year CS student @ Tec de Monterrey. My main areas of
+									interest include computer graphics, artificial intelligence,
+									UX, and web development.
+								</Paragraph>
+								<Row type="flex">
+									<Button
+										href="https://drive.google.com/file/d/1gANwTHqRcKbsLY0w4doSW7wEBnkEyWAH/view?usp=drive_open"
+										rel="noopener"
+										target="_blank"
+									>
+										Resume
+									</Button>
+								</Row>
+							</Col>
+							<Col span={12}>
+								<GioProfilePic></GioProfilePic>
+							</Col>
+						</Row>
+						<Row type="flex" style={{ marginTop: "5px" }}>
+							<Button
+								href="https://www.linkedin.com/in/jorgegiovannetti/"
+								rel="noopener"
+								target="_blank"
+							>
+								<SocialIcon icon={faLinkedin} />
+								LinkedIn
+							</Button>
+							<SpotifyButton
+								href="https://open.spotify.com/artist/3wcBGxsP5MCM5jUPQNoXWC"
+								rel="noopener"
+								target="_blank"
+							>
+								<SocialIcon icon={faSpotify} />
+								Spotify
+							</SpotifyButton>
+							<Button
+								variant="dark"
+								href="https://github.com/JorgeGiovannetti"
+								target="_blank"
+								rel="noopener"
+							>
+								<SocialIcon icon={faGithub} />
+								Github
+							</Button>
+						</Row>
+					</div>
 					<PageHeader title="Work Experience"></PageHeader>
 					<Suspense fallback={<Skeleton active />}>
 						<WorkExperience></WorkExperience>
