04a7f6926ec00fd710b0dc2bf76ea6a32c107e8d
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-03-09 18:20:52 -0600

    Deleted navbar

--- src/views/notfound/index.js
+++ src/views/notfound/index.js
@@ -1,12 +1,10 @@
 import React, { Component, Fragment } from "react";
-import GioNavbar from "../../components/gionavbar";
 import { Layout } from "antd";
 
 class NotFoundPage extends Component {
 	render() {
 		return (
 			<Fragment>
-				<GioNavbar></GioNavbar>
 				<Layout>
 					<div>Oopsie Woopsie, page not found.</div>
 				</Layout>
--- src/views/portfolio/index.js
+++ src/views/portfolio/index.js
@@ -1,10 +1,8 @@
 import React, { Component, Fragment } from "react";
-import GioNavbar from "../../components/gionavbar";
 class PortfolioPage extends Component {
 	render() {
 		return (
 			<Fragment>
-				<GioNavbar></GioNavbar>
 				<div align="middle">Under Construction</div>
 			</Fragment>
 		);
