1e246b41f863ad3d48a17dd9b008286c898cff32
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-03-10 09:23:49 -0600

    Dinamically adds languages

--- src/views/about/components/Projects/components/RepoCard/index.js
+++ src/views/about/components/Projects/components/RepoCard/index.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Card } from "antd";
+import { Button, Card } from "antd";
+import LanguageTag from "../LanguageTag";
 
 const { Meta } = Card;
 
 const RepoCard = ({ data }) => {
 	return (
-		<Card style={{ width: 300 }}>
+		<Card style={{ minWidth: 300 }}>
 			<Meta title={data.node.name} description={data.node.description} />
+			{data.node.languages.edges.map(language => (
+				<LanguageTag tag={language} key={language.node.name} />
+			))}
+			<Button href={data.node.url} target="blank" rel="noopener">
+				Source
+			</Button>
 		</Card>
 	);
 };
